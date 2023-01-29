dd3c5a6f1973847e82d2b7c1d92f9d841f245075
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2023-01-29 19:46:49 +0100

    GUI importado

---
+++ Game/Source/GuiButton.h
@@ -0,0 +1,28 @@
+#ifndef __GUIBUTTON_H__
+#define __GUIBUTTON_H__
+
+#include "GuiControl.h"
+
+#include "Point.h"
+#include "SString.h"
+
+class GuiButton : public GuiControl
+{
+public:
+
+	GuiButton(uint32 id, SDL_Rect bounds, const char* text);
+	virtual ~GuiButton();
+
+	bool Update(float dt);
+	bool Draw(Render* render);
+
+	int mouseX, mouseY;
+	unsigned int click;
+
+	bool canClick = true;
+	bool drawBasic = false;
+
+	uint audioFxId;
+};
+
+#endif // __GUIBUTTON_H__
